bc4663c4aefc5c97f03a1ea07b6cb205e6b31b4b
Ariel Falcón <ariel.falcon@zenerconsulting.com>
2024-10-12 02:34:33 +0200

    loader implementado

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component } from "@angular/core";
 import { TaskTableComponent } from "../tasks-table/tasks-table.component";
 import {TaskFormComponent} from "@/components/task-form/task-form.component";
+import {LoaderComponent} from "@/components/loader/loader.component";
 
 @Component({
   selector: 'app-home',
@@ -8,6 +9,7 @@ import {TaskFormComponent} from "@/components/task-form/task-form.component";
 	imports: [
 		TaskTableComponent,
 		TaskFormComponent,
+		LoaderComponent
 	],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
